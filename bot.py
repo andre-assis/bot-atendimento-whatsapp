d3c0a52cbc8057f3479de834aacf31c09a39f2d8
Andre Pinto <andrepinto.cg@gmail.com>
2024-07-31 14:23:48 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+import os
+from selenium.webdriver.chrome.options import Options
+
+dir_path = os.getcwd()
+chrome_options = Options()
+chrome_options.add_argument(r"user-data-dir=" + dir_path + "profile/whatsapp")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://web.whatsapp.com/')
+time.sleep(120)
